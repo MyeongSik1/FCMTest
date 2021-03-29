9bb3720dfb120b072c25c0d8970371c62f15da95
MyeongSik1 <rkdlqdldkseh@naver.com>
2021-03-29 16:59:39 +0900

    Initial commit

---
+++ app/src/main/java/com/example/fcmtest/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.fcmtest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.iid.FirebaseInstanceId
+import com.google.firebase.messaging.FirebaseMessaging
+import java.io.IOException
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+}
+
